ebccbcebb63ea6ee29accd9e6624c190b0c6a446
nrurnru <nrurnru@gmail.com>
2021-01-19 00:19:57 +0900

    memo 모델 생성

--- api_user/models.py
+++ api_user/models.py
@@ -1,10 +1,24 @@
 from django.db import models
- 
+from django.utils import timezone
+
 class User(models.Model):
     objects = models.Manager() #경고없애기용, 무시가능
+
     user_id = models.CharField(max_length=128, null=False)
     password = models.CharField(max_length=128, null=False)
     address = models.CharField(max_length=256, null=True)
  
     class Meta:
         db_table = "User" #Table이름을 "User"로 설정
+
+class Memo(models.Model):
+    objects = models.Manager() #경고없애기용, 무시가능
+    
+    memo_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    memo_number = models.CharField(max_length=128, null = False)
+    text = models.TextField()
+    createdAt = models.DateField(default=timezone.now)
+    updatedAt = models.DateField(default=timezone.now)
+    isDeleted = models.BooleanField(default=False)
+    isSynced = models.BooleanField(default=False)
+
